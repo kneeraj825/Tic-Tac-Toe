bc64820fd2c6cc975b3fb58fff37ed80ece29d8c
kneeraj825 <kneeraj825@gmail.com>
2020-03-03 10:24:47 +0500

    Added:Checking the winner

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -3,6 +3,9 @@
 echo ____________Welcome_______________
 ROW=3
 COLUMN=3
+LAST=1
+LENGTH=$(($ROW*$COLUMN))
+declare -A index
 #Resetting the board
 function reset()
 {
@@ -15,17 +18,29 @@ function reset()
 	done
 }
 
+function limit()
+{
+	for ((count=0; count<$ROW; count++))
+	do
+		for ((count2=0; count2<$COLUMN; count2++))
+		do
+			index[$count,$count2]=$LAST
+			((LAST++))
+		done
+	done
+}
+
 #check the player
 function checkPlayer()
 {
-randomCheck=$((RANDOM%2))
+	randomCheck=$((RANDOM%2))
 	if (($randomCheck==0))
 	then
 		player=X
 	else
 		player=O
 	fi
-		echo $player
+	echo $player
 }
 
 #checking for the player who play first
@@ -40,7 +55,6 @@ function toss()
 	fi
 }
 
-#game board
 function board()
 {
 	for ((i=0; i<$ROW; i++))
@@ -49,6 +63,77 @@ function board()
 		do
 			printf "|${index[$i,$j]}|"
 		done
-			echo " "
+			echo " "	
 	done
 }
+
+function playerInput()
+{
+	for ((count=0; count<$LENGTH; count++))
+	do
+		board
+		read -p "enter the position" position
+		if [ $position -gt $LENGTH ]
+		then
+			echo "Invalid Number"
+			((count--))
+		else
+			rowNumber=$(($position / $ROW))
+			if [ $(($position % $ROW)) -eq 0 ]
+			then
+				rowNumber=$(($rowNumber-1))
+			fi
+ 				columnNumber=$(($position % $COLUMN))
+				if (($columnNumber == 0))
+				then
+					columnNumber=$(($columnNumber+2))
+				else
+					columnNumber=$(($columnNumber-1))
+				fi
+				index[$rowNumber,$columnNumber]=$player
+				if [ $(firstDiagonal) -eq 1 ]
+				then
+					echo "Winner"
+					return 0
+				fi
+		fi
+	done
+}
+
+#winner function
+function firstDiagonal()
+{
+	if [ ${index[0,0]} == $player ] && [ ${index[0,1]} == $player ] && [ ${index[0,2]} == $player ]
+	then
+		echo 1
+	elif [ ${index[1,0]} == $player ] && [ ${index[1,1]} == $player ] && [ ${index[1,2]} == $player ]
+	then
+		echo 1
+	elif [ ${index[2,0]} == $player ] && [ ${index[2,1]} == $player ] && [ ${index[2,2]} == $player ]
+	then
+		echo 1
+	elif [ ${index[0,0]} == $player ] && [ ${index[1,0]} == $player ] && [ ${index[2,0]} == $player ]
+	then
+		echo 1
+	elif [ ${index[0,2]} == $player ] && [ ${index[1,2]} == $player ] && [ ${index[2,2]} == $player ]
+	then
+		echo 1
+	elif [ ${index[0,1]} == $player ] && [ ${index[1,1]} == $player ] && [ ${index[2,1]} == $player ]
+		then
+		echo 1
+	elif [ ${index[0,0]} == $player ] && [ ${index[1,1]} == $player ] && [ ${index[2,2]} == $player ]
+	then
+		echo 1
+	elif [ ${index[0,2]} == $player ] && [ ${index[1,1]} == $player ] && [ ${index[2,0]} == $player ]	
+	then
+		echo 1
+	else
+	echo 0
+	fi
+}
+reset
+limit
+checkPlayer
+toss
+playerInput
+board
